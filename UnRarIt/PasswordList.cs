d1a38884590b70214ff9da5687f2b567f0bdcda1
Nils Maier <maierman@web.de>
2009-05-18 23:45:23 +0000

    Remove Linq and make dependent on .Net 3.0 only

--- UnRarIt/PasswordList.cs
+++ UnRarIt/PasswordList.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.IO;
 using System.IO.IsolatedStorage;
+using System.Text;
 
 namespace UnRarIt
 {
