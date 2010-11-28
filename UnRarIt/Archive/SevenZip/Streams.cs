225d1ce805288917505abb953dc9fb7fd42e7b9e
Nils Maier <maierman@web.de>
2010-11-28 17:22:27 +0000

    Automatic destruction of incomplete extracts and better buffer management

--- UnRarIt/Archive/SevenZip/Streams.cs
+++ UnRarIt/Archive/SevenZip/Streams.cs
@@ -2,14 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using UnRarIt.Interop;
 
 namespace UnRarIt.Archive.SevenZip
 {
-    internal interface SevenZipStream : IInStream, ISequentialInStream, ISequentialOutStream, IDisposable
+    internal interface ISevenZipCleanupStream
+    {
+        void SetOK();
+    }
+
+    internal interface ISevenZipStream : IInStream, ISequentialInStream, ISequentialOutStream, ISevenZipCleanupStream, IDisposable
     {
     }
 
-    internal class SevenZipNullStream : SevenZipStream
+    internal class SevenZipNullStream : ISevenZipStream
     {
         public uint Read(byte[] data, uint size)
         {
@@ -25,26 +31,26 @@ namespace UnRarIt.Archive.SevenZip
         {
             return size;
         }
-
+        public void SetOK()
+        {
+        }
         public void Dispose()
         {
         }
     }
 
-    internal class SevenZipFileStream : SevenZipStream
+    internal class SevenZipFileStream : ISevenZipStream
     {
+        protected const int BUFFER_SIZE = (1 << 23);
+
         protected FileStream stream;
         public SevenZipFileStream(FileInfo file, FileMode mode)
         {
-            Init(file, mode, FileAccess.Read, 4194304);
+            Init(file, mode, FileAccess.Read, BUFFER_SIZE);
         }
         public SevenZipFileStream(FileInfo file, FileMode mode, FileAccess access)
         {
-            Init(file, mode, access, 4194304);
-        }
-        public SevenZipFileStream(FileInfo file, FileMode mode, FileAccess access, long buffering)
-        {
-            Init(file, mode, access, (int)Math.Min(buffering, 4194304));
+            Init(file, mode, access, BUFFER_SIZE);
         }
         private void Init(FileInfo file, FileMode mode, FileAccess access, int buffering)
         {
@@ -89,10 +95,14 @@ namespace UnRarIt.Archive.SevenZip
                 throw ex;
             }
         }
-        public void Dispose()
+        virtual public void SetOK()
+        {
+        }
+        public virtual void Dispose()
         {
             if (stream != null)
             {
+                stream.Close();
                 stream.Dispose();
                 stream = null;
             }
@@ -100,14 +110,18 @@ namespace UnRarIt.Archive.SevenZip
     }
     internal class SevenZipOutFileStream : SevenZipFileStream
     {
-        const uint PROGRESS_SIZE = 25 * 1024 * 1024;
-        const uint FLUSH_SIZE = 250 * 1024 * 1024;
+        const uint FLUSH_SIZE = 1 << 27;
+
+        private LowPriority lp = null;
 
         long written = 0;
         long fileSize;
         uint flush = FLUSH_SIZE;
-        uint progress = PROGRESS_SIZE;
+        uint progress = BUFFER_SIZE;
         FileInfo file;
+
+        private bool ok = false;
+
         public SevenZipOutFileStream(FileInfo aFile, long aSize)
             : base(aFile, FileMode.Create, FileAccess.ReadWrite)
         {
@@ -118,31 +132,73 @@ namespace UnRarIt.Archive.SevenZip
                 stream.SetLength(fileSize);
                 stream.Flush();
             }
+            if (fileSize > BUFFER_SIZE)
+            {
+                lp = new LowPriority();
+            }
         }
         override public uint Write(byte[] data, uint size)
         {
             uint rv = base.Write(data, size);
-            flush -= Math.Min(rv, flush);
-            if (flush == 0)
-            {
-                stream.Flush();
-                flush = FLUSH_SIZE;
-            }
+            Flush(rv);
             written += rv;
-            
+
             if (ProgressHandler != null)
             {
                 progress -= Math.Min(rv, progress);
                 if (progress == 0)
                 {
-                    ProgressHandler(this, file, written, fileSize);
-                    progress = PROGRESS_SIZE;
+                    if (ProgressHandler != null)
+                    {
+                        ExtractProgressEventArgs eventArgs = new ExtractProgressEventArgs(file, written, fileSize);
+                        ProgressHandler(this, eventArgs);
+                        if (!eventArgs.ContinueOperation)
+                        {
+                            throw new IOException("User canceled");
+                        }
+                    }
+                    progress = BUFFER_SIZE;
                 }
             }
-
             return rv;
+        }
+
+        private void Flush(uint written)
+        {
+            flush -= Math.Min(written, flush);
+            if (flush == 0)
+            {
+                stream.Flush();
+                flush = FLUSH_SIZE;
+                GC.Collect(1, GCCollectionMode.Optimized);
+            }
         }
         public long Written { get { return written; } }
         public event ExtractProgressHandler ProgressHandler;
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            if (lp != null)
+            {
+                lp.Dispose();
+                lp = null;
+            }
+            if (!ok)
+            {
+                try
+                {
+                    file.Delete();
+                }
+                catch (Exception)
+                {
+                    // no op
+                }
+            }
+        }
+        public override void SetOK()
+        {
+            ok = true;
+        }
     }
 }
