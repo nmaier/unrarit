0913b545b4fb852895ffc40bfcaed3fa88d9bda8
Nils Maier <maierman@web.de>
2013-03-09 21:57:31 +0100

    Bump version

--- UnRarIt/Properties/AssemblyInfo.cs
+++ UnRarIt/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("13.03.04.0")]
-[assembly: AssemblyFileVersion("13.03.04.0")]
+[assembly: AssemblyVersion("13.03.09.0")]
+[assembly: AssemblyFileVersion("13.03.09.0")]
 [assembly: NeutralResourcesLanguageAttribute("")]
